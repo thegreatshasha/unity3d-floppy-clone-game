ff13701daa575a670e9f5e14da79e9afffcafa61
Shashwat Kumar <thegreatshasha@gmail.com>
2016-02-07 06:36:06 +0530

    adding *

---
+++ Assets/Scripts/FacebookShare.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FacebookShare : MonoBehaviour {
+	
+	void Update () {
+        if(isTouched()) {
+            FacebookManager.Share("Check out my Floppy Clone greatness!", "I have a high score of " + (PlayerPrefs.GetInt("high_score", 0)).ToString() + " points.  Can you beat me?", "Open source Flappy Bird type game created with Unity3D", "https://raw.githubusercontent.com/nraboy/Floppy-Clone/master/Assets/Icons/android.png", "https://github.com/nraboy/Floppy-Clone");
+        }
+	}
+
+    public bool isTouched() {
+        bool result = false;
+        if(Input.touchCount == 1) {
+            if(Input.touches[0].phase == TouchPhase.Ended) {
+                Vector3 wp = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
+                Vector2 touchPos = new Vector2(wp.x, wp.y);
+                if (collider2D == Physics2D.OverlapPoint(touchPos)) {
+                    result = true;
+                }
+            }
+        }
+        if(Input.GetMouseButtonUp(0)) {
+            Vector3 wp = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 mousePos = new Vector2(wp.x, wp.y);
+            if (collider2D == Physics2D.OverlapPoint(mousePos)) {
+                result = true;
+            }
+        }
+        return result;
+    }
+
+}
---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Title : MonoBehaviour {
+
+	void Start () {
+        guiText.fontSize = Mathf.RoundToInt(Camera.main.pixelHeight / 14f);
+		HTTP.Request someRequest = new HTTP.Request("get", "http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=44db6a862fba0b067b1930da0d769e98");
+		someRequest.Send( ( request ) => {
+			// parse some JSON, for example:
+			JSONObject thing = new JSONObject( request.response.Text );
+			Debug.Log(thing);
+		});
+	}
+
+}
